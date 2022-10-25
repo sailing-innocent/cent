a506ccd31f5908ecb51d9ee06be949cf58b07072
sailing-innocent <sailing-innocent@foxmail.com>
2022-10-25 15:08:57 +0800

    pytorch start

---
+++ hello/pytorch/eval_fmnist.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets 
+from torchvision.transforms import ToTensor
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork()
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot"
+]
+
+model.eval()
+x, y = test_data[12][0], test_data[12][1]
+with torch.no_grad():
+    pred = model(x)
+    print(pred)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+            
