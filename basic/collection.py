9a176ed57e4f5ac41d10cb6238f4e57ae1a3bb97
sailing-innocent <sailing-innocent@foxmail.com>
2022-11-01 16:21:16 +0800

    add pybind11

---
+++ basic/collection.py
@@ -0,0 +1,28 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+# namedtuple was introduced into python since v2.6 for creating some classes with a few attributes but no methods
+# like Database Index
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+print(beer_card)
+
+deck = FrenchDeck()
+print(len(deck))
+
+    
---
+++ pybind11/hello/hello.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("D:/workspaces/my-research-00/libs/sail/build/Debug")
+
+import addpy
+print(addpy.add(1,2))
