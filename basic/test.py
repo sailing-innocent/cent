401cb4d5de3c9adf566e6f27102b3f9d869fc453
sailing-innocent <sailing-innocent@foxmail.com>
2022-11-03 09:54:44 +0800

    homework main program done

--- basic/test.py
+++ basic/test.py
@@ -1,17 +1,26 @@
-from mpl_toolkits import mplot3d
 import numpy as np
-import matplotlib.pyplot as plt
-#求向量积(outer()方法又称外积)
-x = np.outer(np.linspace(-2, 2, 30), np.ones(30))
-print(x.shape)
-#矩阵转置
-y = x.copy().T 
-#数据z
-z = np.cos(x ** 2 + y ** 2)
-#绘制曲面图
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-# 调用plot_surface()函数
-ax.plot_surface(x, y, z,cmap='viridis', edgecolor='none')
-ax.set_title('Surface plot')
-plt.show()
+
+items = np.ones([5,3])
+for i in range(5):
+    for j in range(3):
+        items[i][j] = i + j
+        
+print(items)
+
+count = items.shape[0]
+n = items.shape[1]
+sumlist = np.zeros(n)
+
+for i in range(count):
+    item = items[i,:] 
+    for j in range(n):
+        sumlist[j] = sumlist[j] + item[j] 
+
+print(sumlist)
+
+for i in range(count):
+    item = items[i,:]
+    for j in range(n):
+        item[j] = item[j]/sumlist[j] 
+
+print(items)
--- main.py
+++ main.py
@@ -6,6 +6,32 @@ import random
 from sklearn.model_selection import KFold
 csvpath = "D:/workspaces/njudaily/MachineLearning/aqidataset.csv"
 
+
+def lsm(x_samples, y_samples):
+    count = x_samples.shape[0]
+    n = x_samples.shape[1]
+    m = y_samples.shape[1]
+
+    #init theta 
+    theta = np.zeros([n,m], dtype=float)
+    # build params
+    sumxxt = 0
+    sumxyt = np.zeros([n,m], dtype=float)
+
+    for i in range(count):
+        x = x_samples[i,:].reshape([n,1])
+        y = y_samples[i,:].reshape([m,1])
+        sumxxt = sumxxt + np.matmul(x,x.transpose())
+        sumxyt = sumxyt + np.matmul(x,y.transpose())
+    # gradiant update
+    alpha = 0.001 # learning rate
+    steps = 50
+    for step in range(steps):
+        theta = theta - alpha *( np.matmul(sumxxt,theta) - sumxyt)
+        # print(theta)
+
+    return theta
+
 # Read data from CSV file and store them into numpy array
 def readdata():
     data = []
@@ -29,6 +55,39 @@ def readdata():
 
     return clean_data, n
 
+def normalize(items):
+    #L1 Normalize
+    count = items.shape[0]
+    n = items.shape[1]
+    sumlist = np.zeros(n)
+
+    for i in range(count):
+        item = items[i,:] 
+        for j in range(n):
+            sumlist[j] = sumlist[j] + item[j] 
+
+    # print(sumlist)
+
+    for i in range(count):
+        item = items[i,:]
+        for j in range(n):
+            item[j] = item[j]/sumlist[j] 
+    
+    return items
+
+def test(x_test, y_truth, theta):
+    sumres = 0
+    count = x_test.shape[0]
+    n = x_test.shape[1]
+    m = y_truth.shape[1]
+
+    for i in range(count):
+        x = x_test[i,:].reshape(n,1)
+        y = y_truth[i,:].reshape(m,1)
+        res = y - np.matmul(theta.transpose(), x)
+        sumres = sumres + np.matmul(res.transpose(), res)
+    
+    return sumres
 # class model
 # method train(data,ids)
 # method test(data,ids)
@@ -41,7 +100,17 @@ def main():
 
     # data: [328,8+1+1]
     data, n = readdata()
+
     # print(data.shape)
+    # test_data = data[0:2,:]
+    x = data[:,0:8].reshape(n,8)
+    y = data[:,8].reshape(n,1)
+    x = normalize(x)
+    y = normalize(y)
+    # print(x.shape)
+    # print(y.shape)
+
+    # print(theta.shape)
 
     # seperate test and train data
 
@@ -51,14 +120,21 @@ def main():
 
     split_list = []
     kf = KFold(n_splits=folds, shuffle=True)
-    # for trainIDs, testIDs in kf.split(data):
-        # here trainIDs is the list of indicies that will be used for training
-        # and testIDs is similiar
-    
-    print(i)
-    print(folds, gap)
-
-
+    result = 0
+    for trainIDs, testIDs in kf.split(data):
+        #construct train and test list
+        NTrain = trainIDs.shape[0]
+        NTest = testIDs.shape[0]
+        train_x = np.array([x[i] for i in trainIDs]).reshape(NTrain,8)
+        train_y = np.array([y[i] for i in trainIDs]).reshape(NTrain,1)
+        theta = lsm(train_x, train_y)
+        # print("theta shape: ", theta.shape)
+        test_x = np.array([x[i] for i in testIDs]).reshape(NTest,8)
+        test_y = np.array([y[i] for i in testIDs]).reshape(NTest,1)
+        
+        res = test(test_x, test_y, theta)
+        result = result + res
+    print(result)
 
 if __name__ == "__main__":
     main()
