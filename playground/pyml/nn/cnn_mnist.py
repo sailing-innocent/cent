b90256aaf400c2bff0f9891e8ccf8a611b9e23a9
unknown <colored_black@outlook.com>
2023-01-06 16:42:44 +0800

    cnn first impl

--- playground/pyml/nn/cnn_mnist.py
+++ playground/pyml/nn/cnn_mnist.py
@@ -7,6 +7,8 @@ from PIL import Image
 import matplotlib.pyplot as plt 
 import os
 from torchvision import datasets, transforms, utils 
+import torch.nn.functional as F
+import torch.optim as optim
 
 ## Prepare Data MNIST
 
@@ -64,6 +66,67 @@ def debug_one_batch():
     plt.imshow(img)
     plt.show()
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64*7*7, 1024)
+        self.fc2 = nn.Linear(1024,512)
+        self.fc3 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        
+        x = x.view(-1, 64 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+
+def main():
+    net = CNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    train_accs = []
+    train_loss = []
+    test_accs = []
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = net.to(device)
+
+    for epoch in range(3):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() # every 100 batch output a mean loss
+            if (i%100==99):
+                print("[{},{}] loss {}".format(epoch+1, i+1, running_loss/100))
+                running_loss = 0.0
+            train_loss.append(loss.item())
+    
+            correct = 0
+            total = 0
+            _, predicted = torch.max(outputs.data, 1)
+            total = labels.size(0)
+            correct = (predicted == labels).sum().item()
+            train_accs.append(100*correct/total)
+    print("Train Fin")
+
+    PATH = "./mnist.pth"
+    torch.save(net.state_dict(), PATH)
+
 if __name__ == "__main__":
     # debug()
-    debug_one_batch()
+    # debug_one_batch()
+    main()
