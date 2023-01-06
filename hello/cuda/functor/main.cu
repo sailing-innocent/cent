d1e121113beeff643c82a1ca3b60312cb3f620b5
unknown <colored_black@outlook.com>
2023-01-06 16:06:25 +0800

    add functor realization

--- hello/cuda/functor/main.cu
+++ hello/cuda/functor/main.cu
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
+template <class T>
+struct CudaAllocator {
+    using value_type = T;
+    CudaAllocator() = default;
+    template<class _Other>
+    constexpr CudaAllocator(const CudaAllocator<_Other>&) noexcept {}
+
+    T *allocate(size_t size) {
+        T *ptr = nullptr;
+        cudaMallocManaged(&ptr, size * sizeof(T));
+        return ptr;
+    }
+
+    void deallocate(T *ptr, size_t size = 0) {
+        cudaFree(ptr);
+    }
+
+    // plain old data
+    template <class ...Args>
+    void constructor(T *p, Args &&...args) {
+        if constexpr (!(sizeof...(Args) == 0 && std::is_pod_v<T>))
+        {
+            ::new((void*)p) T(std::forward<Args>(args)...);
+        }
+    }
+};
+
 template<class Func>
 __global__ void parallel_for(int n, Func func) {
     for (int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -17,7 +44,10 @@ struct MyFunctor {
 
 int main() {
     int n = 10;
-    parallel_for<<<2,8>>>(n, MyFunctor{});
+    // parallel_for<<<2,8>>>(n, MyFunctor{});
+    parallel_for<<<2,8>>>(n, [] __device__ (int i) {
+        printf("lambda Number %d \n", i);
+    });
     cudaDeviceSynchronize();
     return 0;
 }
